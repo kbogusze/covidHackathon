6a50d230ab44013aebd301785dd36aebd507ea48
BOGUSZEWSKI Krzysztof <k.boguszewski@soprabanking.com>
2020-04-25 13:16:29 +0200

    Deal endpoint find by customer ID, Example how to use collection to search in DB

--- src/main/java/com/itds/covid/hackathon/covid/models/DealRepository.java
+++ src/main/java/com/itds/covid/hackathon/covid/models/DealRepository.java
@@ -2,6 +2,10 @@ package com.itds.covid.hackathon.covid.models;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 //@RepositoryRestResource(collectionResourceRel = "deals", path = "deals")
 public interface DealRepository extends MongoRepository<Deal, String> {
+
+    List<Deal> findByCustomerId(String customerId);
 }
