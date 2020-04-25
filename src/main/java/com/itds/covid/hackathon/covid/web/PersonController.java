952218e2241537c42ec79eda2afa58f4cb518dc4
BOGUSZEWSKI Krzysztof <k.boguszewski@soprabanking.com>
2020-04-25 11:22:21 +0200

    Person rest Controller

--- src/main/java/com/itds/covid/hackathon/covid/web/PersonController.java
+++ src/main/java/com/itds/covid/hackathon/covid/web/PersonController.java
@@ -3,12 +3,12 @@ package com.itds.covid.hackathon.covid.web;
 import com.itds.covid.hackathon.covid.models.Person;
 import com.itds.covid.hackathon.covid.models.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/persons")
@@ -21,9 +21,25 @@ public class PersonController {
     @Autowired
     private PersonRepository repository;
 
-    @GetMapping("/{id}")
-    public List<Person> getAllPersons(@PathVariable String id) {
+    @GetMapping("")
+    public List<Person> getAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> get(@PathVariable String id) {
+        Optional<Person> deal = repository.findById(id);
+        if (deal.isPresent()) {
+            return new ResponseEntity<>(deal.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @PostMapping(path = "", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Person> add(@RequestBody Person object) {
+        Person insertedObject = repository.insert(object);
+        return new ResponseEntity<>(insertedObject, HttpStatus.CREATED);
     }
 }
 
