84dd4aec00ae777ff5e65fbffcee20a4fae35f74
BOGUSZEWSKI Krzysztof <k.boguszewski@soprabanking.com>
2020-04-26 09:38:01 +0200

    Endpoint for deal detail view

---
+++ src/main/java/com/itds/covid/hackathon/covid/models/DealDetailProxy.java
@@ -0,0 +1,22 @@
+package com.itds.covid.hackathon.covid.models;
+
+public class DealDetailProxy {
+    Company company;
+    User user;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
--- src/main/java/com/itds/covid/hackathon/covid/web/DealViewController.java
+++ src/main/java/com/itds/covid/hackathon/covid/web/DealViewController.java
@@ -1,9 +1,6 @@
 package com.itds.covid.hackathon.covid.web;
 
-import com.itds.covid.hackathon.covid.models.Deal;
-import com.itds.covid.hackathon.covid.models.DealRepository;
-import com.itds.covid.hackathon.covid.models.Document;
-import com.itds.covid.hackathon.covid.models.DocumentRepository;
+import com.itds.covid.hackathon.covid.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +20,12 @@ public class DealViewController {
 
     @Autowired
     private DealRepository dealRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @CrossOrigin
     @GetMapping("/dealview/deals")
@@ -50,6 +53,35 @@ public class DealViewController {
         if (document.isPresent()){
 //            document.get().setContent(Base64.getEncoder().encodeToString(document.get().getContent().getData()));
             return new ResponseEntity<>(document.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @CrossOrigin
+    @GetMapping("/dealview/details/{id}")
+    public ResponseEntity<DealDetailProxy> getDealDetails(@PathVariable String id) {
+
+        DealDetailProxy dealDetailProxy = new DealDetailProxy();
+        Optional<Deal> byId = dealRepository.findById(id);
+
+        if (byId.isPresent()){
+            String userId = byId.get().getUserId();
+            Optional<User> byId1 = userRepository.findById(userId);
+            if (byId1.isPresent()){
+                byId1.get().setPassword(null);
+                byId1.get().setUsername(null);
+                byId1.get().setId(null);
+                byId1.get().setCardIdNumber(null);
+                dealDetailProxy.setUser(byId1.get());
+
+                Optional<Company> byUserId = companyRepository.findByUserId(userId);
+                byUserId.get().setUserId(null);
+                dealDetailProxy.setCompany(byUserId.get());
+            }
+
+
+            return new ResponseEntity<>(dealDetailProxy, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
         }
